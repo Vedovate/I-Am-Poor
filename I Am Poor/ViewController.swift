a7d1044998623412724ee1a95afa972ad63660d6
Leonardo Vedovate <lvedovate@outlook.com>
2023-03-07 22:26:16 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Leonardo Vedovate on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
